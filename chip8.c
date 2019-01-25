15586e3d759929246e50a3c2d2b47ea344765b60
rafascar <scarduellir@gmail.com>
2019-01-25 16:04:42 -0200

    Implement main emulation loop

--- chip8.c
+++ chip8.c
@@ -45,6 +45,151 @@ int main(int argc, char **argv) {
     cpu_reset(file);
     fclose(file);
 
+
+    /* begin emulation loop */
+    int running = 1;
+    while (running) {
+        /* Fetch instruction.
+         * CHIP-8 instructions are 2-bytes, but it has a byte-addressable memory
+         * thus, we need to get two consecutive bytes to fech a single 
+         * instruction, hence the PC register is incremented twice. 
+         * Instructions are big-endian; so the most significant bits are shifted
+         * left and OR'd with the least significant to obtain the full instr. */
+        uint16_t instr = (uint16_t)memory[reg_PC] << 8 | memory[reg_PC+1];  
+        reg_PC = reg_PC + 2;
+        /* The first hexadecimal digit of an opcode dictates which instruction 
+         * needs to be executed; in some cases, the last hex digit is also 
+         * needed. */
+        switch (instr & 0xF000) {
+            /* 0NNN (not implemented), 00E0, 00EE */
+            case 0x0000:
+                switch (instr & 0x000F) {
+                    case 0x0:
+                        break;
+                    case 0xE:
+                        break;
+                    default:
+                        abort();
+                        break;
+                }
+                break;
+            /* 1NNN */
+            case 0x1000:
+                break;
+            /* 2NNN */
+            case 0x2000:
+                break;
+            /* 3XNN */
+            case 0x3000:
+                break;
+            /* 4XNN */
+            case 0x4000:
+                break;
+            /* 5XY0 */
+            case 0x5000:
+                break;
+            /* 6XNN */
+            case 0x6000:
+                break;
+            /* 7XNN */
+            case 0x7000:
+                break;
+            /* 8XY0, 8XY1, 8XY2, 8XY3, 8XY4, 8XY5, 8XY6, 8XY7, 8XYE */
+            case 0x8000:
+                switch (instr & 0x000F) {
+                    case 0x0:
+                        break;
+                    case 0x1:
+                        break;
+                    case 0x2:
+                        break;
+                    case 0x3:
+                        break;
+                    case 0x4:
+                        break;
+                    case 0x5:
+                        break;
+                    case 0x6:
+                        break;
+                    case 0x7:
+                        break;
+                    case 0xE:
+                        break;
+                    default:
+                        fprintf(stderr, "chip8: invalid opcode\n");
+                        abort();
+                        break;
+                }
+                break;
+            /* 9XY0 */
+            case 0x9000:
+                break;
+            /* ANNN */
+            case 0xA000:
+                break;
+            /* BNNN */
+            case 0xB000:
+                break;
+            /* CXNN */
+            case 0xC000:
+                break;
+            /* DXYN */
+            case 0xD000:
+                break;
+            /* EX9E, EXA1 */
+            case 0xE000:
+                switch (instr & 0x000F) {
+                    case 0xE:
+                        break;
+                    case 0x1:
+                        break;
+                    default:
+                        fprintf(stderr, "chip8: invalid opcode");
+                        abort();
+                        break;
+                }
+                break;
+            /* FX07, FX0A, FX18, FX1E, FX29, FX33, FX15, FX55, FX65 */
+            case 0xF000:
+                switch (instr & 0x000F) {
+                    case 0x0007:
+                        break;
+                    case 0x000A:
+                        break;
+                    case 0x0008:
+                        break;
+                    case 0x000E:
+                        break;
+                    case 0x0009:
+                        break;
+                    case 0x0003:
+                        break;
+                    case 0x0005:
+                        switch (instr & 0x00F0) {
+                            case 0x0010:
+                                break;
+                            case 0x0050:
+                                break;
+                            case 0x0060:
+                                break;
+                            default:
+                                fprintf(stderr, "chip8: invalid opcode");
+                                abort();
+                                break;
+                        }
+                    default:
+                        fprintf(stderr, "chip8: invalid opcode");
+                        abort();
+                        break;
+                }
+                break;
+            default:
+                fprintf(stderr, "chip8: invalid opcode");
+                abort();
+                break;
+        }
+    }
+
     return 0;
 }
 
